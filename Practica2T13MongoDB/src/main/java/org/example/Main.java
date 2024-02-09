3942c097591f448b5e7d3f323fb4f24a3f796605
SrBu <srbucj@gmail.com>
2024-02-09 12:53:17 +0100

    Se han añadido comentarios a las clases.

--- Practica2T13MongoDB/src/main/java/org/example/Main.java
+++ Practica2T13MongoDB/src/main/java/org/example/Main.java
@@ -5,9 +5,17 @@ import org.bson.Document;
 import java.awt.*;
 import java.util.List;
 
+// Clase principal del programa.
 public class Main {
+    // Método principal que se ejecuta al iniciar la aplicación.
     public static void main(String[] args) {
+        // Crea una instancia de la clase Presentacion, que parece ser una clase personalizada.
+        // Se le proporciona la URL de la base de datos ("mongodb://localhost:27017") y el nombre de la base de datos ("myDatabase").
         Presentacion presentacion = new Presentacion("mongodb://localhost:27017", "myDatabase");
+
+        // Llama al método mostrarMenu() de la instancia de Presentacion.
+        // Este método contiene la lógica para mostrar un menú interactivo de la base de datos MongoDB.
         presentacion.mostrarMenu();
     }
 }
+
--- Practica2T13MongoDB/src/main/java/org/example/MongoDBManager.java
+++ Practica2T13MongoDB/src/main/java/org/example/MongoDBManager.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
@@ -14,35 +12,43 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+// Clase que gestiona las operaciones para la base de datos de MongoDB.
 public class MongoDBManager {
+    // Conexión a la base de datos.
     private MongoClient mongoClient;
     private MongoDatabase database;
 
+    // Constructor que inicializa la conexión a la base de datos MongoDB.
     public MongoDBManager(String connectionString, String databaseName) {
         mongoClient = MongoClients.create(connectionString);
         database = mongoClient.getDatabase(databaseName);
     }
 
+    // Método para insertar un documento en una colección específica.
     public void insertDocument(String collectionName, Document document) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         collection.insertOne(document);
     }
 
+    // Método para actualizar documentos en una colección basado en una consulta.
     public void updateDocument(String collectionName, Document query, Document update) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         collection.updateMany(query, update);
     }
 
+    // Método para reemplazar documentos en una colección basado en una consulta.
     public void replaceDocument(String collectionName, Document query, Document replacement) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         collection.replaceOne(query, replacement);
     }
 
+    // Método para eliminar documentos en una colección basado en una consulta.
     public void deleteDocument(String collectionName, Document query) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         collection.deleteOne(query);
     }
 
+    // Método para obtener todos los documentos de una colección.
     public List<Document> getAllDocuments(String collectionName) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         FindIterable<Document> findIterable = collection.find();
@@ -55,6 +61,7 @@ public class MongoDBManager {
         return documents;
     }
 
+    // Método para encontrar documentos en una colección basado en una consulta.
     public List<Document> findDocuments(String collectionName, Document query) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         FindIterable<Document> findIterable = collection.find(query);
@@ -67,84 +74,32 @@ public class MongoDBManager {
         return documents;
     }
 
+    // Método para eliminar todos los documentos de una colección.
     public void deleteAllDocuments(String collectionName) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         collection.deleteMany(new Document());
     }
 
-    // Utilidad 1: Obtener documentos con un campo específico igual a un valor dado
+    // Utilidad 1: Obtener documentos con un campo específico igual a un valor dado.
     public List<Document> getDocumentsByFieldValue(String collectionName, String fieldName, Object value) {
         Document query = new Document(fieldName, value);
         return findDocuments(collectionName, query);
     }
 
-    // Utilidad 2: Obtener documentos con un campo específico que contenga un valor dado
+    // Utilidad 2: Obtener documentos con un campo específico que contenga un valor dado.
     public List<Document> getDocumentsByFieldContains(String collectionName, String fieldName, String value) {
         Document query = new Document(fieldName, new Document("$regex", value).append("$options", "i"));
         return findDocuments(collectionName, query);
     }
-    // Método para ejecutar agregaciones pipeline
+
+    // Método para ejecutar agregaciones pipeline.
     public List<Document> runAggregate(String collectionName, List<Document> pipeline) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         return collection.aggregate(pipeline).into(new ArrayList<>());
     }
 
-    // Método para exportar una colección a un archivo CSV
+    // Método para exportar una colección a un archivo CSV.
     public void exportCollectionToCSV(String collectionName, String fileName) {
         MongoCollection<Document> collection = database.getCollection(collectionName);
         try (PrintWriter writer = new PrintWriter(new File(fileName + ".csv"))) {
-            MongoCursor<Document> cursor = collection.find().iterator();
-            while (cursor.hasNext()) {
-                Document doc = cursor.next();
-                writer.println(doc.toJson());
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Método para exportar una colección a un archivo JSON
-    public void exportCollectionToJSON(String collectionName, String fileName) {
-        MongoCollection<Document> collection = database.getCollection(collectionName);
-        try (PrintWriter writer = new PrintWriter(new File(fileName + ".json"))) {
-            MongoCursor<Document> cursor = collection.find().iterator();
-            while (cursor.hasNext()) {
-                Document doc = cursor.next();
-                writer.println(doc.toJson());
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Método para importar datos desde un archivo CSV a una colección
-    public void importCSVToCollection(String collectionName, String filePath) {
-        MongoCollection<Document> collection = database.getCollection(collectionName);
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                Document doc = Document.parse(line);
-                collection.insertOne(doc);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Método para importar datos desde un archivo JSON a una colección
-    public void importJSONToCollection(String collectionName, String filePath) {
-        MongoCollection<Document> collection = database.getCollection(collectionName);
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                Document doc = Document.parse(line);
-                collection.insertOne(doc);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-
-
+            MongoCursor<Docume
